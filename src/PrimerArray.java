ce616f2d4e9c612f4728c8e6621fc981ab142a76
Andreea <andelenasamur@gmail.com>
2022-06-06 13:09:53 +0200

    First commit

---
+++ src/PrimerArray.java
@@ -0,0 +1,10 @@
+public class PrimerArray {
+	public static void main (String arg[]) {
+		String nombres[] = {
+				"Juan", "Maria", "Antonio", "Victoria"
+		};
+		nombres[2] = "Alejandro";
+		System.out.println(nombres[0]);
+	}
+	
+}
